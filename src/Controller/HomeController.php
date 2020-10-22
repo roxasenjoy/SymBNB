e44e951816d48b9f0b496c1295627769fa04b15a
roxasenjoy <andrew.mahe14@gmail.com>
2020-10-22 15:15:00 +0200

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        $prenom = "andrew";
+
+        return $this->render(
+            'home/index.html.twig',
+            [
+                'prenom' => $prenom
+            ]
+        );
+    }
+}
