526e347636be81eff5592a01c9426a7df3276120
Andrew Mahé <andrew.mahe14@gmail.com>
2020-10-27 10:53:15 +0100

    Ajouter des annonces + message flash

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -5,8 +5,9 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Form\AdType;
 use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -35,14 +36,32 @@ class AdController extends AbstractController
      *
      * @Route("/ads/new", name="ads_create")
      *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
      * @return Response
      */
-    public function create(){
-
+    public function create(Request $request, EntityManagerInterface $manager){
         $ad = new Ad();
 
         $form = $this->createForm(AdType::class, $ad);
 
+        $form->handleRequest($request);
+
+
+        $this->addFlash(
+            'success',
+            "L'annonce <strong> {$ad->getTitle()} </strong> a bien été enregistrée ! "
+        );
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($ad);
+            $manager->flush();
+
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+
+        }
         return $this->render('ad/new.html.twig', [
             'form' => $form->createView()
         ]);
